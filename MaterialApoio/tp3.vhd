586311ab59e6684ab4154789a879930441bc46cb
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2023-06-06 19:08:09 +0000

    fin

--- MaterialApoio/tp3.vhd
+++ MaterialApoio/tp3.vhd
@@ -39,7 +39,7 @@ begin
     if reset = '1' then
       data <= (others => '0');
     elsif rising_edge(clock) then
-      data <= din & data(7 downto 1);--perguntar se isso pode
+      data <= din & data(7 downto 1);
     end if;
   end process;
 
@@ -92,19 +92,41 @@ begin
   
   found <= (match(0) OR match(1) OR match(2) OR match(3));
 
-  program(0) <= '1' when EA = '001' else '0';
-  program(1) <= '1' when EA = '010' else '0';
-  program(2) <= '1' when EA = '011' else '0';
-  program(3) <= '1' when EA = '100' else '0';
+  program(0) <= '1' when EA = A else '0';
+  program(1) <= '1' when EA = B else '0';
+  program(2) <= '1' when EA = C else '0';
+  program(3) <= '1' when EA = D else '0';
   
   --  registradores para ativar as comparações
+  process(clock,reset)
+  begin 
+  if reset='1' OR EA = zerar then
+      sel(0)<='0';
+      sel(1)<='0';
+      sel(2)<='0';
+      sel(3)<='0';
+    if rising_edge(clock) begin 
+      if EA = A then
+        sel(0)<='1';
+      if EA = B then
+        sel(1)<='1';
+      if EA = C then
+        sel(2)<='1';
+      if EA = B then
+        sel(3)<='1';
+    end if;
+  end if;
+  end if;
+  end if;
+  end if;
+ end process;
 
-
+ 
 
   --  registrador para o alarme interno
     process (clock, reset)
   begin
-    if reset = '1' then
+    if reset = '1' OR EA = zerar then
       alarm_int <= '0';
     elsif rising_edge(clock) then
       if EA = '101' then
@@ -131,18 +153,29 @@ begin
     begin
       case EA is
           when S0 => 
-              if prog='001' then PE <=A;
-              else PE <= S0; end if;
-          when A => 
-              if prog='010' then PE <=B;
-              else PE <=; end if;
-          when B => 
-              if prog='010' then PE <=B; --else PE <= S3; end if;
-          when C => if prog='011' then PE <=C; --else PE <= S1; end if;
-          when D => if prog='011' then PE <=D; --else PE <= S1; end if;
-
-
-      end case;
+              if prog="001" then PE <= A;
+              else
+                
+              elsif prog="010" then PE <=B;
+              elsif prog="011" then PE <=C;
+              elsif prog="100" then PE <=D;
+              elsif prog="101" then PE <= buscando;
+              end if; 
+          when buscando =>
+                if found = '1' then PE <= bloqueio;
+                elsif prog = "111" then PE <= zerar;
+                end if;
+          when bloqueio =>
+                if prog = "110" then PE <= buscando;
+                elsif prog = "111" then PE<= zerar;
+                end if;
+          when A => PE<= s0;
+          when B => PE<= s0;
+          when C => PE<= s0;
+          when D => PE<= s0;
+          when zerar => PE<= s0;
+                
+     end case;
 end process;
 
   -- SAIDAS
@@ -151,8 +184,6 @@ end process;
   numero <= ('11' when match(3) = '1' else
              '10' when match(2) = '1' else
              '01' when match(1) = '1' else
-             '00' when match(0) = '1');--normalmente a gente não faz o último else completo
-             --só que a gente só quer que passe se algum deles for '1'
-             --acho que se não fizer isso ele dá erro
+             '00' );
 
 end architecture;
